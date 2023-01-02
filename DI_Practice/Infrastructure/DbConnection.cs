a6f540d3f37f8376ba11e549f75a809ef15451bb
Damodhar jadhao <Damodhar.jadhao@emtecinc.com>
2023-01-02 10:27:27 +0530

    Connection string Updated

--- DI_Practice/Infrastructure/DbConnection.cs
+++ DI_Practice/Infrastructure/DbConnection.cs
@@ -10,7 +10,7 @@
         public DbConnection( IConfiguration configuration)
         {           
             _configuration = configuration;
-            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+            _connectionString = @"server=PUN-NB-LE3B6ND; database=DapperAdvance; Integrated Security=True;";
         }
         public SqlConnection DatabaseConnection()
         {
