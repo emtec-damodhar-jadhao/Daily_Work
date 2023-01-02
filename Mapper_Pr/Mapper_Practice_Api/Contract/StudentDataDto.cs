5356019c31c6373cc064431ee9261feb024bfe35
Damodhar jadhao <Damodhar.jadhao@emtecinc.com>
2023-01-02 10:32:23 +0530

    Mapper Practice Project Uploaded

---
+++ Mapper_Pr/Mapper_Practice_Api/Contract/StudentDataDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Contract
+{
+    public class StudentDataDto
+    {
+        public string Name { get; set; }=string.Empty;
+        public string Education { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+    }
+}
---
+++ Mapper_Pr/Mapper_Practice_Api/Domain/StudentData.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+    public class StudentData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }=String.Empty;
+        public string Education { get; set; }=String.Empty;
+        public string City { get; set; } = String.Empty;
+    }
+}
